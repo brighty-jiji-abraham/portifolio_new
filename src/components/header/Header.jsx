a1d19157cca38e620bca35e26d80a5e409fa0544
Brighty Jiji Abraham <brightymct@gmail.com>
2025-01-25 15:25:06 +0530

    Add files via upload

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,134 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
+import Typed from "typed.js";
 import './Header.css';
 
 const Header = () => {
+  const typedElement = useRef(null);
+  const [animationState, setAnimationState] = useState(0);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen); // Toggles the menu state
+  };
+
+  useEffect(() => {
+    // First, type "Welcome 👋" without cursor
+    const welcomeOptions = {
+      strings: ["Welcome 👋 to "], // Texts to be typed
+      typeSpeed: 50, // Faster typing speed
+      backSpeed: 30, // Faster backspacing
+      backDelay: 700, // Faster delay before backspacing
+      startDelay: 300, // Shorter start delay
+      loop: false, // We don't want this part to loop
+      showCursor: false, // Hide cursor for the welcome text
+      onComplete: () => setAnimationState(1), // Set state to trigger next part after Welcome
+    };
+
+    const typedWelcome = new Typed(typedElement.current, welcomeOptions);
+
+    return () => {
+      typedWelcome.destroy();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (animationState === 1) {
+      // Type B J A after a short pause
+      setTimeout(() => {
+        setAnimationState(2);
+      }, 200); // Delay before starting B J A
+    }
+
+    if (animationState === 2) {
+      // Type B J A (Step 1)
+      setTimeout(() => {
+        setAnimationState(3); // Delay before showing the parts after typing B J A
+        // Erase everything with backspace animation
+        const eraseOptions = {
+          strings: [""], // Empty string to backspace to
+          typeSpeed: 50, // Typing speed for backspacing
+          backSpeed: 10, // Backspacing speed
+          fadeOut: true, // Enable fade out effect
+          fadeOutClass: 'typed-fade-out', // CSS class for fade out effect
+          fadeOutDelay: 300, // Delay before fade out starts
+          showCursor: false, // Hide cursor during backspacing
+        };
+
+        const typedErase = new Typed(typedElement.current, eraseOptions);
+      }, 500);
+
+      // Type "righty"
+      setTimeout(() => {
+        const typedRighty = new Typed("#righty", {
+          strings: ["righty"],
+          typeSpeed: 40,
+          backSpeed: 30,
+          showCursor: false,
+        });
+        setAnimationState(4); // Set state to show "righty" after typing
+      }, 800);
+
+      // Type "iji"
+      setTimeout(() => {
+        const typedIji = new Typed("#iji", {
+          strings: ["iji"],
+          typeSpeed: 50,
+          backSpeed: 30,
+          showCursor: false,
+        });
+        setAnimationState(5); // Set state to show "iji" after typing
+      }, 900);
+
+      // Type "raham"
+      setTimeout(() => {
+        const typedRaham = new Typed("#raham", {
+          strings: ["raham"],
+          typeSpeed: 50,
+          backSpeed: 30,
+          showCursor: false,
+          onComplete: () => setAnimationState(6), // Erase everything after "BJA" is typed
+        });
+      }, 1000);
+    }
+  }, [animationState]);
+
   return (
-    <header className="focus-in-contract-bck">
-      <h2>Welcome 👋</h2>
-      <nav>
-        <Link to="/" className="nav-link">About Me</Link>
-        <Link to="/skills" className="nav-link">Skills</Link>
-        <Link to="/projects" className="nav-link">Projects</Link>
-        <Link to="/contact" className="nav-link">Contact</Link>
+    <header>
+      <h2 className="focus-in-contract-bck">
+        <span ref={typedElement}></span> {/* Welcome text */}
+        <span className={`typed-content ${animationState >= 2 ? 'show' : ''}`}>
+          B
+          <span className={`typed-expanded ${(animationState >= 3 && animationState >= 4) ? 'expand' : ''}`}>
+            <span id="righty" className={`part part1 ${animationState >= 4 ? 'show' : ''}`}></span>
+          </span>
+          J
+          <span className={`typed-expanded ${(animationState >= 3 && animationState >= 5) ? 'expand' : ''}`}>
+            <span id="iji" className={`part part2 ${animationState >= 5 ? 'show' : ''}`}></span>
+          </span>
+          A
+          <span className={`typed-expanded ${(animationState >= 3 && animationState >= 6) ? 'expand' : ''}`}>
+            <span id="raham" className={`part part3 ${animationState >= 6 ? 'show' : ''}`}></span>
+          </span>
+        </span>
+      </h2>
+
+      {/* Navigation Menu */}
+      <nav className={`nav ${menuOpen ? 'open' : ''}`}>
+        <div className="hamburger" onClick={toggleMenu}>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
+        <div className={`nav-links focus-in-contract-bck ${menuOpen ? 'show' : ''}`}>
+          <Link to="/" className="nav-link">About Me</Link>
+          <Link to="/skills" className="nav-link">Skills</Link>
+          <Link to="/projects" className="nav-link">Projects</Link>
+          <Link to="/contact" className="nav-link">Contact</Link>
+        </div>
       </nav>
     </header>
   );
-}
+};
 
 export default Header;
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -4,7 +4,7 @@ import { FaServer } from "react-icons/fa6";
 import { BiLogoPhp } from "react-icons/bi";
 import { AiFillApi } from "react-icons/ai";
 import { GiMusicalKeyboard, GiMeshNetwork } from "react-icons/gi";
-import { TbBrandOffice ,TbRobot, TbBrandCSharp } from "react-icons/tb";
+import { TbBrandOffice, TbRobot, TbBrandCSharp } from "react-icons/tb";
 import { MdBuild, MdDataObject } from 'react-icons/md';
 import { GrSwift } from "react-icons/gr";
 import { SiCplusplus, SiAdobephotoshop, SiAffinitydesigner, SiAffinityphoto, SiAdobelightroom, SiAdobepremierepro, SiAdobeaftereffects, SiBlender, SiMysql, SiSparkar, SiGithub, SiUnity } from "react-icons/si";
@@ -25,50 +25,52 @@ const Skills = () => {
 
     return (
         <>
-            <h2 className="text-4xl font-extrabold mb-6 text-center">My Skills</h2>
-            <div className="skill-list">
-                {/* Programming Languages */}
-                <ul className="skill-card bg-gradient-to-r from-blue-400 to-blue-700 shadow-lg rounded-lg p-6">
-                    <strong className="text-2xl font-semibold text-white mb-6">Programming Languages</strong>
-                    <li className='clang'><SiCplusplus className="icon" /> C/C++</li>
-                    <li className='csharplang'><TbBrandCSharp className="icon" /> C# (C Sharp)</li>
-                    <li className='swiftlang'><GrSwift className="icon" /> Swift</li>
-                    <li className='phplang'><BiLogoPhp className="icon" /> PHP</li>
-                    <li className='javalang'><FaJava className="icon" /> Java</li>
-                    <li className='jslang'><FaJsSquare className="icon" /> JavaScript</li>
-                    <li className='pythonlang'><FaPython className="icon" /> Python</li>
-                    <li className='nodejslang'><FaNodeJs className="icon" /> Node.js</li>
-                </ul>
+            <div className='fade-in'>
+                <h2 className="text-4xl font-extrabold mb-6 text-center">My Skills</h2>
+                <div className="skill-list">
+                    {/* Programming Languages */}
+                    <ul className="skill-card bg-gradient-to-r from-blue-400 to-blue-700 shadow-lg rounded-lg p-6">
+                        <strong className="text-2xl font-semibold text-white mb-6">Programming Languages</strong>
+                        <li className='clang'><SiCplusplus className="icon" /> C/C++</li>
+                        <li className='csharplang'><TbBrandCSharp className="icon" /> C# (C Sharp)</li>
+                        <li className='swiftlang'><GrSwift className="icon" /> Swift</li>
+                        <li className='phplang'><BiLogoPhp className="icon" /> PHP</li>
+                        <li className='javalang'><FaJava className="icon" /> Java</li>
+                        <li className='jslang'><FaJsSquare className="icon" /> JavaScript</li>
+                        <li className='pythonlang'><FaPython className="icon" /> Python</li>
+                        <li className='nodejslang'><FaNodeJs className="icon" /> Node.js</li>
+                    </ul>
 
-                {/* Video Editing / Photography */}
-                <ul className="skill-card bg-gradient-to-r from-green-400 to-green-700 shadow-lg rounded-lg p-6">
-                    <strong className="text-2xl font-semibold text-white mb-6">Video Editing / Photography</strong>
-                    <li className='ae'><SiAdobeaftereffects className="icon" /> Adobe After Effects</li>
-                    <li className='pr'><SiAdobepremierepro className="icon" /> Adobe Premiere Pro</li>
-                    <li className='ps'><SiAdobephotoshop className="icon" /> Adobe Photoshop</li>
-                    <li className='lr'><SiAdobelightroom className="icon" /> Adobe Lightroom</li>
-                    <li className='afd'><SiAffinitydesigner className="icon" /> Affinity Designer</li>
-                    <li className='afp'><SiAffinityphoto className="icon" /> Affinity Photo</li>
-                </ul>
+                    {/* Video Editing / Photography */}
+                    <ul className="skill-card bg-gradient-to-r from-green-400 to-green-700 shadow-lg rounded-lg p-6">
+                        <strong className="text-2xl font-semibold text-white mb-6">Video Editing / Photography</strong>
+                        <li className='ae'><SiAdobeaftereffects className="icon" /> Adobe After Effects</li>
+                        <li className='pr'><SiAdobepremierepro className="icon" /> Adobe Premiere Pro</li>
+                        <li className='ps'><SiAdobephotoshop className="icon" /> Adobe Photoshop</li>
+                        <li className='lr'><SiAdobelightroom className="icon" /> Adobe Lightroom</li>
+                        <li className='afd'><SiAffinitydesigner className="icon" /> Affinity Designer</li>
+                        <li className='afp'><SiAffinityphoto className="icon" /> Affinity Photo</li>
+                    </ul>
 
-                {/* Other Skills */}
-                <ul className="skill-card bg-gradient-to-r from-purple-400 to-purple-700 shadow-lg rounded-lg p-6">
-                    <strong className="text-2xl font-semibold text-white mb-6">Other Skills</strong>
-                    <li><TbBrandOffice className="icon" /> Microsoft Office</li>
-                    <li><TbRobot className="icon" /> Machine Learning</li>
-                    <li><AiFillApi className="icon" /> API Integration</li>
-                    <li><MdDataObject className="icon" /> Object-Oriented Programming</li>
-                    <li><SiMysql className="icon" /> MySQL</li>
-                    <li><GiMeshNetwork className="icon" /> Neural Networks</li>
-                    <li><MdBuild className="icon" /> Problem-solving</li>
-                    <li><MdBuild className="icon" /> Technical Support</li>
-                    <li><FaServer className="icon" /> Web Hosting</li>
-                    <li><SiBlender className="icon" /> Blender</li>
-                    <li><SiUnity className="icon" /> Unity</li>
-                    <li><SiSparkar className="icon" /> Spark AR</li>
-                    <li><SiGithub className="icon" /> GitHub</li>
-                    <li><GiMusicalKeyboard className="icon" /> FL Studio</li>
-                </ul>
+                    {/* Other Skills */}
+                    <ul className="skill-card bg-gradient-to-r from-purple-400 to-purple-700 shadow-lg rounded-lg p-6">
+                        <strong className="text-2xl font-semibold text-white mb-6">Other Skills</strong>
+                        <li><TbBrandOffice className="icon" /> Microsoft Office</li>
+                        <li><TbRobot className="icon" /> Machine Learning</li>
+                        <li><AiFillApi className="icon" /> API Integration</li>
+                        <li><MdDataObject className="icon" /> Object-Oriented Programming</li>
+                        <li><SiMysql className="icon" /> MySQL</li>
+                        <li><GiMeshNetwork className="icon" /> Neural Networks</li>
+                        <li><MdBuild className="icon" /> Problem-solving</li>
+                        <li><MdBuild className="icon" /> Technical Support</li>
+                        <li><FaServer className="icon" /> Web Hosting</li>
+                        <li><SiBlender className="icon" /> Blender</li>
+                        <li><SiUnity className="icon" /> Unity</li>
+                        <li><SiSparkar className="icon" /> Spark AR</li>
+                        <li><SiGithub className="icon" /> GitHub</li>
+                        <li><GiMusicalKeyboard className="icon" /> FL Studio</li>
+                    </ul>
+                </div>
             </div>
         </>
     );
