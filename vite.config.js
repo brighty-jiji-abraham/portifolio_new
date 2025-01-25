51907ec5041b24509b93c32664b964c73e85b991
Brighty Jiji Abraham <brightymct@gmail.com>
2025-01-25 16:20:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 // vite.config.js
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   css: {
     postcss: './postcss.config.js', // Ensure Vite uses the correct PostCSS config file
   },
+  base: 'portifolio_new', // Replace REPO_NAME with your GitHub repository name
+  plugins: [react()],
 });
